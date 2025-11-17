8b9a8ce83c71bc819c1a0574d393bfca1f0fcc9c
jsalcedocarrion-ai <jsalcedocarrion@gmail.com>
2025-11-17 16:36:47 -0500

    Update api-rest.js

--- backend/src/api-rest.js
+++ backend/src/api-rest.js
@@ -91,9 +91,9 @@ const pool = new Pool({
 /////////////////////////////////////////////
 
 app.get('/estadisticas/usuarios', async (req, res) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, Origin, X-Requested-With');
+    //res.header('Access-Control-Allow-Origin', req.headers.origin);
+  //res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  //res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, Origin, X-Requested-With');
   const { fecha_desde, fecha_hasta } = req.query;
 
   // Validar parámetros obligatorios

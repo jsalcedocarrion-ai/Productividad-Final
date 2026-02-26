c8f898731e68a64f840bbd1598bbf7c5483d3107
jsalcedocarrion-ai <jsalcedocarrion@gmail.com>
2026-02-26 11:58:30 -0500

    Update script.js

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -683,7 +683,7 @@ function cerrarSesion() {
 
     // Cambiar vistas
     document.getElementById('main-window').style.display = 'none';
-    document.getElementById('login-window').style.display = 'flex';
+    document.getElementById('login-window').style.display = 'block';
 }
 
 // cerrarSesion definido en index.html
